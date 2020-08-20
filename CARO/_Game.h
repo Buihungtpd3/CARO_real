09615f8db801d2d04386dd49da1fd2d95a13feed
Buihungtpd3 <68337200+Buihungtpd3@users.noreply.github.com>
2020-08-20 10:43:52 +0700

    Update _Game.h
    :+1:

--- CARO/_Game.h
+++ CARO/_Game.h
@@ -6,31 +6,31 @@
 #include <map>
 class _Game : public _Board {
 private:
-	_Board* _b;
-	bool _turn;
-	int _x, _y;
-	int _command;
-	bool _loop;
-	int _i, _j;
-	int turnX, turnO;
+	_Board* _b;// bàn cờ
+	bool _turn;// lượt đi
+	int _x, _y;// tọa độ
+	int _command;// phím nhập vào 
+	bool _loop;// biến quản lý lập 
+	int _i, _j;// tọa độ trên bàn cờ
+	int turnX, turnO;// lượt của X vs lượt của Y
 public:
 	_Game(int, int, int);
 	~_Game();
 public:
-	int getCommand();
-	bool isContinue();
-	char waitKeyBoard();
-	char askContinue();
-	void startGame();
-	void exitGame();
-	int processFinish();
-	bool processCheckBoard();
-	void moveRight();
-	void moveLeft();
-	void moveUp();
-	void moveDown();
+	int getCommand();// trả về phím nhập vào 
+	bool isContinue();// Biến loop á
+	char waitKeyBoard();// chờ nhập vào 
+	char askContinue();// đi tiếp hay không 
+	void startGame();// Bắt đầu game 
+	void exitGame();// thay đổi biến loop
+	int processFinish();// Trả về biến kiểu int, 0: hòa, 1: O thắng, -1: X thằng. 2: tiếp tục game
+	bool processCheckBoard();// Kiểm tra ô đó đánh được hay không ?
+	void moveRight();// Sang phải trên bàn cờ
+	void moveLeft();// Sang trái trên bàn cờ
+	void moveUp();// Đi lên trên bàn cờ
+	void moveDown();// Đi xuống trên bàn cờ
 	
-	void loadGame();
+	void loadGame();// Tải game từ file
 public:
 	//Choi voi may 
 	void playWithAi_Hard();
